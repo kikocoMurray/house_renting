f7d43e82e5b27eceaa41fc003c84344e7e8044c4
MurrayChenMBP2023 <murraychenmbp2023@SAMurrayChenMBP2023.local>
2023-06-15 10:49:33 +0800

    a

---
+++ sevices/SportsBook.js
@@ -0,0 +1,20 @@
+export const getPlaceBet = () => {
+    return new Promise((resolve,reject)=>{
+
+    })
+}
+export const getPlaceBec = () => {
+    return new Promise((resolve,reject)=>{
+
+    })
+}
+export const getPlaceBea = () => {
+    return new Promise((resolve,reject)=>{
+
+    })
+}
+export const getPlaceBep = () => {
+    return new Promise((resolve,reject)=>{
+
+    })
+}
